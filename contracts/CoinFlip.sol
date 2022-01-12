ed5c0e50ca62f1c9b346e3a866ba9ceb885ab08e
markibanez <mark.ibanez@gmail.com>
2022-01-12 17:11:15 +0800

    Added state variables with corresponding  getter and updater functions

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.11;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.2/contracts/access/AccessControl.sol";
 
 contract CoinFlip is AccessControl {
-    uint _minBet = 10000000000000000; // default minimum bet is 0.01
+    uint _minBet = 10000000000000000; // default minimum bet is 0.01 ETH in wei units
+    uint _maxBet = 5000000000000000000; // default maximum bet is 5 ETH in wei units
+    uint _playerWinPercentage = 50; // default winning percentage is 50%
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); // set owner as admin
@@ -13,5 +15,17 @@ contract CoinFlip is AccessControl {
     modifier onlyAdmin() {
         // this modifier restricts function calls to only succeed when caller has admin role
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender));
+        _;
     }
+
+    // state getters and updaters    
+    function setMinBet(uint amountWei) public onlyAdmin { _minBet = amountWei; }
+    function getMinBet() public view onlyAdmin returns (uint) { return _minBet; }
+    function setMaxBet(uint amountWei) public onlyAdmin { _maxBet = amountWei; }
+    function getMaxBet() public view onlyAdmin returns (uint) { return _maxBet; }
+    function setPlayerWinPercentage(uint percentage) public onlyAdmin { 
+        require(percentage <= 100, "Player win percentage must be 100% or less");
+        _playerWinPercentage = percentage; 
+    }
+    function getWinPercentage() public view onlyAdmin returns (uint) { return _playerWinPercentage; }
 }
