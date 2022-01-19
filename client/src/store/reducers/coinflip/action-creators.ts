564ac3a5c304afa924b4963c8ccce8debe0ac37e
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-19 20:41:27 +0800

    Added sample code to get transaction specific event logs

--- client/src/store/reducers/coinflip/action-creators.ts
+++ client/src/store/reducers/coinflip/action-creators.ts
@@ -45,10 +45,24 @@ export const CoinflipActionCreators = {
       const contract = coinflipSelectors.selectContract(store);
       if (contract) {
         try {
-          const test = await contract.flip(side, {
+          const startBlock = await contract.provider.getBlockNumber(); // set current block number as starting block for query
+          const tx = await contract.flip(side, {
             value: betSize.toString(),
           });
-          console.log(test);
+          console.log(contract);
+
+          await tx.wait();
+
+          const playerAddress = await contract.signer.getAddress();
+          const filter = await contract.filters.playerFlipped(null, playerAddress);
+          const query = await contract.queryFilter(filter, startBlock, 'latest');
+          console.log(query); // parse results
+          // query will return an array of event logs
+          // in 90 percent of all cases it should only return one event log
+          // but in case it returns multiple logs, use the one with the largest block number
+          // relevant data is inside the args object (for example if args.amountWon = 0, then player has lost)
+          // transaction matching can be refined further by using logEvent.getTransaction()
+          // which contains transaction hash and transaction value (amount bet)
         } catch (err) {
           console.log(err);
           dispatch(CoinflipActionCreators.setError(['Unexpected Error']));
