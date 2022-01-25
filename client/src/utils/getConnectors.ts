6fd739ccad135304d49a7c8f296a1023d1d120c6
Andrew Strigaliov <strigaliov4working@gmail.com>
2022-01-25 09:26:11 +0700

    Updated connectors

--- client/src/utils/getConnectors.ts
+++ client/src/utils/getConnectors.ts
@@ -1,5 +1,5 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 
 export const getConnectors = new InjectedConnector({
-  supportedChainIds: [1337, 31337, 1, 80001, 4],
+  supportedChainIds: [137, 80001, 4],
 });
