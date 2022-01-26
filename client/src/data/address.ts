dfa4b4f6f6ad0c459379232caf17aa03ed5e5d34
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-26 11:02:00 +0800

    Updated polygon testnet address

--- client/src/data/address.ts
+++ client/src/data/address.ts
@@ -1,2 +1,2 @@
 // export const address = '0xa54C6e665e131C437fE3a66B697bb5b7db5CB8b4'; // rinkeby contract address
-export const address = '0x942eAfC117B58CBF4EE7af45833710CB7087B525'; // polygon testnet address
+export const address = '0x63468a06B3d195f6E33899d818Acf78f89A48Fe3'; // polygon testnet address
