c9ba791b9dff3031c12fb3387766ce57a3778c15
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-28 12:16:43 +0800

    Change back ether matic conversion factor to 1 for test deploy

--- client/src/utils/formatEther.ts
+++ client/src/utils/formatEther.ts
@@ -2,7 +2,8 @@ import { formatEther, parseEther } from 'ethers/lib/utils';
 import { BigNumber } from 'ethers';
 
 // In a future it will be better to use api to fetch all the recent exchange rates. Need to think about it
-export const ONE_ETHER_IN_MATIC = 1500;
+// export const ONE_ETHER_IN_MATIC = 1500; // polygon mainnet
+export const ONE_ETHER_IN_MATIC = 1; // polygon testnet
 // 1623.76;
 
 export const weiToEth = (wei: BigNumber) => formatEther(wei);
